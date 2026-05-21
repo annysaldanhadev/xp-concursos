1a63ccd9d2f7b3e8dd139ee5ebdce66ec6467da4
Delete User <cseunome@gmail.com>
2026-05-20 22:09:18 -0400

    feat(auth): adiciona listener de estado de autenticação do `Firebase`

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import React, { useState, useEffect} from "react";
+import { auth } from "../services/firebaseConfig";
+import { onAuthStateChanged, User } from "firebase/auth";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import RecoverPasswordScreen from "../screens/RecoverPasswordScreen";
-import UpdatePasswordScreen from "../screens/UpdatePasswordScreen";
+import HomeScreen from "../screens/HomeScreen";
 
 import { RootStackParamList } from "../types/navigation";
 
@@ -18,8 +19,25 @@ export default function AppNavigator() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="RecoverPassword" component={RecoverPasswordScreen} />
-        <Stack.Screen name="UpdatePassword" component={UpdatePasswordScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export function Routes() {
+  const [user, setUser] = React.useState<User | null>(null);
+
+  useEffect(() => {
+    const subscriber = onAuthStateChanged(auth, setUser);
+
+    return () => subscriber();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      {user ? <AppNavigator /> : <AppNavigator />}
+    </NavigationContainer>
+  );
+}
+  
